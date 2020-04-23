57395af1ee0ca0fb43683ea8505dd233e41e754a
Jeffery Zhang <zhenyu051988@gmail.com>
2020-04-23 03:09:49 -0700

    test guessword component when there is guessed word

--- jotto/src/components/guess-words/GuessWords.jsx
+++ jotto/src/components/guess-words/GuessWords.jsx
@@ -5,6 +5,27 @@ const GuessWords = ({ guessWords }) => {
   let contents;
   if (!guessWords.length) {
     contents = <span data-test="guess-instruction">Try to guess a word</span>;
+  } else {
+    const guessWordsRows = guessWords.map((word, index) => (
+      <tr key={index} data-test='guessd-word'>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    contents = (
+      <div data-test="guessd-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
   return <div data-test="component-guessed-words">{contents}</div>;
 };
--- jotto/src/components/guess-words/GuessWords.test.js
+++ jotto/src/components/guess-words/GuessWords.test.js
@@ -30,4 +30,28 @@ describe("if there are no words guessed", () => {
   });
 });
 
-describe("if there are words guessed", () => {});
+describe("if there are words guessed", () => {
+  const guessWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 },
+  ];
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessWords });
+  });
+  test("render component withour error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  test("render guess word section", () => {
+    const guessWordsNode = findByTestAttr(wrapper, "guessd-words");
+    expect(guessWordsNode.length).toBe(1);
+  });
+
+  test("correct number of guess word", () => {
+    const guessWordNodes = findByTestAttr(wrapper, "guessd-word");
+    expect(guessWordNodes.length).toBe(guessWords.length);
+  });
+});
