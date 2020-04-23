b9088028e4ecb33e430349dffac873ec792c8815
Jeffery Zhang <zhenyu051988@gmail.com>
2020-04-23 00:50:08 -0700

    extract the enzyme setup to setupTests.js

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,14 +1,11 @@
 import React from "react";
 import App from "./App";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import { findByTestAttr } from "./utils/testUtils";
 
 const setup = (props = {}) => {
   return shallow(<App {...props} />);
 };
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test("renders component without error", () => {
   const wrapper = setup();
